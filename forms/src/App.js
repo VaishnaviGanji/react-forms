10b12c91a38650aef8a80d9cbb3414e16f35ecff
VaishnaviGanji <vaishnaviganji24@gmail.com>
2021-02-21 18:40:08 +0530

    1578196

---
+++ forms/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import HomeComponent from './components/HomeComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <HomeComponent/>
+      </div>
+  );
+}
+
+export default App;
